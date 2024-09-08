e6bca6f373989b8f434754b250da8c839e963329
BinaryProgram <github.saklain@gamil.com>
2024-09-08 15:36:53 +0600

    All are done

--- js/reuse.js
+++ js/reuse.js
@@ -1,25 +1,39 @@
-function hidePlay(elementId) {
+function hideElementById(elementId) {
   const getPlay = document.getElementById(elementId);
   getPlay.classList.add("hidden");
 }
-function showPlayGround(elementId) {
+function showElementById(elementId) {
   const getPlayGround = document.getElementById(elementId);
   getPlayGround.classList.remove("hidden");
 }
-
+// highlight the play field key backgroundColor
 function setKeyBackgroundColor(elementId) {
   const currentKey = document.getElementById(elementId);
   currentKey.style.backgroundColor = "#FFA500";
 }
-
+// Clear Right key background and continue the game
+function removeBackgroundColor(elementId) {
+  const currentKey = document.getElementById(elementId);
+  currentKey.style.backgroundColor = "";
+}
+// update score and life
+function getById(elementId) {
+  const value = document.getElementById(elementId);
+  let currentScore = parseInt(value.innerText);
+  return currentScore;
+}
+function setById(elementId, value) {
+  const setValue = document.getElementById(elementId);
+  setValue.innerText = value;
+}
+// This function generate random letter
 function getRandomAlphabet() {
-  // step-1 -> set alphabet
+  // step-1 set alphabet
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const convertedArrayAlphabet = alphabet.split("");
-  // generate random integer number to iterate over the array
+  // step-2 generate random integer number to iterate over the array
   const randomInteger = Math.round(Math.random() * 25);
   // step-3 store random alphabet and return
   const randomAlphabet = convertedArrayAlphabet[randomInteger];
   return randomAlphabet;
 }
-
--- js/script.js
+++ js/script.js
@@ -5,14 +5,44 @@ home section
 step-2: remove hidden from play-ground section
 */
 // Note -> we will do it more efficiently when we learn react.
-// function letsPlay(){
-//     // step-1
-//     const getPlay = document.getElementById('play');
-//     getPlay.classList.add('hidden');
-//     // step-2
-//     const getField = document.getElementById("play-ground");
-//     getField.classList.remove('hidden');
-// }
+/*
+function letsPlay(){
+    // step-1
+    const getPlay = document.getElementById('play');
+    getPlay.classList.add('hidden');
+    // step-2
+    const getField = document.getElementById("play-ground");
+    getField.classList.remove('hidden');
+}
+*/
+
+// Detect keyUp from keyboard
+function detectKeyboard(event) {
+  // Get the Key pressed and check right or wrong key pressed
+  if(event.key === 'Escape') gameOver();
+  const youPress = event.key.toUpperCase();
+  const showKey = document.getElementById("present-text");
+  const currentKey = showKey.innerText;
+  if (youPress === currentKey) {
+    // update score
+    const currentScore = getById("update-score");
+    let updateScore = currentScore + 1;
+    setById("update-score", updateScore);
+    removeBackgroundColor(currentKey.toLocaleLowerCase());
+    startGame();
+  } else {
+    // update life
+    const currentLife = getById("update-life");
+    let updateLife = currentLife - 1;
+    setById("update-life", updateLife);
+
+      if (updateLife === 0) {
+        gameOver();
+        
+      }
+  }
+}
+document.addEventListener("keyup", detectKeyboard);
 
 // little bit smart way to perform above task
 function startGame() {
@@ -27,7 +57,28 @@ function startGame() {
   setKeyBackgroundColor(letter);
 }
 function letsPlay() {
-  hidePlay("play");
-  showPlayGround("play-ground");
+  // hide everything except play-ground
+  hideElementById("play");
+  hideElementById("final-score");
+  // reset previous life and score
+  setById("update-life", 5);
+  setById("update-score", 0);
+  setById("finalScore-value", 0);
+  showElementById("play-ground");
   startGame();
+}
+
+// When score hit zero then game over
+function gameOver() {
+  hideElementById("play-ground");
+  showElementById("final-score");
+
+  // update final score
+  const getScore = getById('update-score');
+  setById('finalScore-value',getScore);
+
+  // reset previous field key backgroundColor
+  const getFieldText = document.getElementById('present-text');
+  let getFieldTextLetter = getFieldText.innerText.toLowerCase();
+  removeBackgroundColor(getFieldTextLetter);
 }
